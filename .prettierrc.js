6c08eecaf1b819c63bbba807f94715b45bbe0da9
Ben Christel <ben.christel@gmail.com>
2022-06-25 20:41:41 -0700

    Use .prettierrc.js from Druthers

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: false,
+  trailingComma: "all",
+
+  // If any object property name requires quotes, quote them
+  // all.
+  quoteProps: "consistent",
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+}
